b08e32ab039ed6b83cd853fd114086d8509883d3
OrcClaSH <orcaclash@gmail.com>
2020-05-25 14:23:25 +0900

    The main code for solving task 2.4.8

---
+++ stepik_lesson_2.4.8.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import math
+
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+browser = webdriver.Chrome()
+try:
+    browser.get('http://suninjuly.github.io/explicit_wait2.html')
+
+    price = WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, 'price'), '100')
+    )
+
+    browser.find_element(By.ID, 'book').click()
+
+    x = browser.find_element(By.ID, 'input_value').text
+    browser.find_element(By.ID, 'answer').send_keys(calc(x))
+    browser.find_element(By.ID, 'solve').click()
+
+    print('Ответ: ', browser.switch_to.alert.text.split(': ')[-1])
+finally:
+    browser.quit()
